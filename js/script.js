5a8f27b20f1fe5dffcf6672b48219fb84e6bdd1b
mst141 <it8anken8@gmail.com>
2020-03-25 22:50:35 +0900

    最初のコミット

---
+++ js/script.js
@@ -0,0 +1,68 @@
+//document(webpage) is ready to do this function.
+$(document).ready(function(){
+
+    $('#slides').superslides({
+        animation:"fade", 
+        play: 5000, 
+        pagination: false 
+    });
+
+    var typed = new Typed(".typed" , {
+        strings: ["Mio Araki." , "Nonoka Yamamoto." , "Hidetaka Eguchi.", "Hirofumi Ishii." ],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+    $(window).scroll(function() {
+
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+             });
+         
+
+        }
+    });
+
+    $(".counter").each(function() {
+        var element = $(this);
+        var endVal = parseInt(element.text());
+
+        element.countup(endVal);
+    });
+
+});
