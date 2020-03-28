50661be4d421b4225e66d339161996c7326ed81a
mst141 <it8anken8@gmail.com>
2020-03-28 20:20:24 +0900

    スクロール時のnavバー固定

--- js/script.js
+++ js/script.js
@@ -65,4 +65,20 @@ $(document).ready(function(){
         element.countup(endVal);
     });
 
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation(){
+
+        var body = $("body");
+        if($(window).scrollTop() >= navTop){
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        }else{
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+        }
+    }
 });
